a2bb55b3fc67d1d4f0914acbd4c33e4ca45bc0ab
Tanmay-Anand <tanmayanand2002@gmail.com>
2026-02-01 00:21:18 +0530

    Initial project setup

---
+++ src/main/java/com/tanmay/secure_e_commerce/SecureECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.secure_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/secure_e_commerce/SecureECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.secure_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
